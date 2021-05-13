65e38b66b768f5e45c41373d1994cc182e5ca707
Alex Volochai <smithloch00@gmail.com>
2021-05-13 17:51:54 +0300

    may 5.13

--- js/1.js
+++ js/1.js
@@ -246,3 +246,78 @@
 
 // ----------------------------------
 
+// let user = {
+//     name: "John",
+//     age: 30,
+// };
+
+// let key = prompt('what?', '');
+// alert(user[key]);
+
+// alert("nam" in user) // false
+
+// for (let key in user) {
+//     alert(key);
+// }
+
+// ----------------------------------
+
+// let user = new Object();
+
+// user.name = "John";
+// user.surname = "Smith";
+// user.name = "Pete";
+
+// delete user.name;
+
+// ----------------------------------
+
+// let schedule = {};
+
+// function isEmpty() {
+//     for (let key in schedule) {
+//         return false;
+//     }
+//     return true;
+// }
+
+// alert( isEmpty(schedule) ); 
+
+// schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) );
+
+// ----------------------------------
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// };
+
+// let sum = 0; 
+
+// for (let key in salaries) {
+//     sum += salaries[key];
+// }
+
+// alert(sum);
+
+// ----------------------------------
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
+
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+        if (typeof obj[key] == 'nubmer') {
+            obj[key] *= 2;
+        }
+    }
+}
+
+multiplyNumeric(menu); 
+console.log(menu);
